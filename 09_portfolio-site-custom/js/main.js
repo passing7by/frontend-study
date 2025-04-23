72e50b212a7a6925640bb79d1b78b6846986dfe7
passing7by <passing7by.naver.com>
2025-04-23 19:45:46 +0900

    frontend 수업 (js - 모달, 실습 - portfolio site 제작)

--- 09_portfolio-site-custom/js/main.js
+++ 09_portfolio-site-custom/js/main.js
@@ -7,8 +7,17 @@ spyEls.forEach(function (spyEl) {
   // 메소드 체이닝
   new ScrollMagic.Scene({ // 감시할 장면 추가 및 옵션 지정
     triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
-    triggerHook: 0.4 // 화면의 50% 지점에서 보여짐 여부 감시(0~1사이 지정)
+    triggerHook: 0.6 // 화면의 50% 지점에서 보여짐 여부 감시(0~1사이 지정)
   })
   .setClassToggle(spyEl, 'underline') // 요소가 화면에 보이면 show 클래스 추가
   .addTo(controller); // 컨트롤러에 장면을 할당(필수!!) - 라이브러리에서 지정한 문법 깊게 이해할 필요x
   });
+
+const modalCloseBtn = document.querySelector('.btn-close');
+const modalBtn = document.querySelector('.btn-close');
+
+const modal = document.querySelector('#readmeModal');
+
+modalCloseBtn.addEventListener('click', function () {
+  modal.style.display = 'flex';
+})
